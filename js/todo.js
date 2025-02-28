350cea8496782aa0766d555f3c9f7f81120f1d4c
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-02-28 10:41:26 +0100

    js alap

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+function getSzint() {
+    var szintek = document.querySelectorAll(".szint");
+
+    for (var i = 0; i < szintek; i++) {
+        if (szintek[i].checked) {
+            return i;
+        }
+    }
+}
+
+function hozzaad() {
+    var tbody = document.querySelector("tbody");
+    var feladat = document.querySelector(".feladat");
+    var szint = getSzint();
+
+    feladat = feladat.trim();
+
+    if (szint == null || feladat == "") {
+        alert("Adjon meg egy érvényes bemenetet!");
+    } else {
+        
+    }
+}
