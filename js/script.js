db40e8b8ccb1639d0b29d63f91ffe5329ed0e582
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-02-28 08:33:51 +0100

    elemek

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function teszt() {
     var jelszo = document.querySelector(".jelszo");
+    var progress = document.querySelector(".progress");
+    var progressBar = document.querySelector(".progress-bar");
     var jelszoSzint = 0;
 }
