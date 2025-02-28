4798ad38f890ecd84b5332ebf2cffafddfe93e6b
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-02-28 08:47:33 +0100

    hossz és kis és nagy betű

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
+const betuk = "aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz";
 function teszt() {
-    var jelszo = document.querySelector(".jelszo");
+    var jelszo = document.querySelector(".jelszo").value;
     var progress = document.querySelector(".progress");
     var progressBar = document.querySelector(".progress-bar");
     var jelszoSzint = 0;
+
+    jelszo = jelszo.trim();
+
+    if (jelszo.length >= 8) {
+        jelszoSzint++;
+    }
+
+    var vege = false;
+    var i = 0;
+    var kisEsNagyBetu = 0;
+
+    while(!vege) {
+        if (jelszo.includes(betuk[i])) {
+            kisEsNagyBetu++;
+        }
+
+        if (jelszo.includes(betuk.toUpperCase()[i])) {
+            kisEsNagyBetu++;
+        }
+
+        if (kisEsNagyBetu == 2) {
+            jelszoSzint++;
+            vege = false;
+        }
+
+        i++;
+    }
 }
