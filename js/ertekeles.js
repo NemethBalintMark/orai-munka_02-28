48e89878010f84d12d931e3f53efa2855dc4338d
Sólyom Attila <112870390+Csattila123@users.noreply.github.com>
2025-02-28 10:04:03 +0100

    kiír

--- js/ertekeles.js
+++ js/ertekeles.js
@@ -1,11 +1,30 @@
 var ertekelesek = [];
+var ertekeles = 0;
 
 function ertekel(clickedValue) {
+    ertekeles = 0;
     var csilagok = document.querySelectorAll("i");
 
     for (var i = clickedValue - 1; i >= 0; i--) {
         csilagok[i].classList.remove("bi-star")
         csilagok[i].classList.add("bi-star-fill");
         csilagok[i].style.color = "yellow";
+        ertekeles++;
+    }
+}
+
+function mentes(){
+    var tbody = document.querySelector("tbody");
+    tbody.innerHTML = ""
+    if (ertekeles == 0){
+        alert("Adjon meg egy értékelést!");
+    } else{
+        ertekelesek.push(ertekeles);
+        for (let i = 0; i < ertekelesek.length; i++){
+            tbody.innerHTML += `<tr>
+                                    <th scope="row">${ertekelesek.length}</th>
+                                    <td>${ertekelesek[i]}</td>
+                                </tr>`
+        }
     }
 }
