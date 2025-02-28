8e3e18350fecfff81682855c7c2b4909d9e30a8c
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-02-28 09:54:02 +0100

    csillag szinezes

--- js/ertekeles.js
+++ js/ertekeles.js
@@ -4,6 +4,8 @@ function ertekel(clickedValue) {
     var csilagok = document.querySelectorAll("i");
 
     for (var i = clickedValue - 1; i >= 0; i--) {
-        csilagok[i].style.backgroundColor = "yellow";
+        csilagok[i].classList.remove("bi-star")
+        csilagok[i].classList.add("bi-star-fill");
+        csilagok[i].style.color = "yellow";
     }
 }
