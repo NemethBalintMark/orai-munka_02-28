4908d4afedaff64d576b1da4e1a167dd658b529d
NemethBalintMark <118978756+NemethBalintMark@users.noreply.github.com>
2025-02-28 09:48:01 +0100

    jav

--- js/ertekeles.js
+++ js/ertekeles.js
@@ -3,7 +3,7 @@ var ertekelesek = [];
 function ertekel(clickedValue) {
     var csilagok = document.querySelectorAll("i");
 
-    for (var i = clickedValue - 1; i <= 0; i--) {
+    for (var i = clickedValue - 1; i >= 0; i--) {
         csilagok[i].style.backgroundColor = "yellow";
     }
 }
